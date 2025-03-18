0fa5f11dc46001aefa76692d73751a6958dbc6c5
liyo <serotoninoy@gmail.com>
2025-03-18 16:52:43 +0700

    log color

--- main.ts
+++ main.ts
@@ -3,7 +3,13 @@ import { TextLineStream } from "@std/streams/text-line-stream";
 const input = Deno.stdin.readable.pipeThrough(new TextDecoderStream())
   .pipeThrough(new TextLineStream());
 
-console.log('Welcome!! Let\'s start with "hello"\n  Type: "bye" to exit\n');
+console.log(
+  "Welcome!! Let's start with %chello\n  %cType: %cbye %cto exit\n",
+  "color: yellow;",
+  "color: white;",
+  "color: yellow;",
+  "color: white;",
+);
 await Deno.stdout.write(new TextEncoder().encode("> "));
 
 for await (const line of input) {
